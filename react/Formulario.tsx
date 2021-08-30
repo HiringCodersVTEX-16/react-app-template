c35a3824cebd3423b79773a8cf3b7684b23a278d
Luiz Henrique Cudo <ineedsomespace@gmail.com>
2021-08-30 19:17:44 -0300

    first commit

---
+++ react/Formulario.tsx
@@ -0,0 +1,47 @@
+import React, { useState, FormEvent } from 'react'
+
+import styles from './styles.css';
+
+const Formulario = () => {
+
+    const [nameField, setNameField] = useState('')
+    const [emailField, setEmailField] = useState('')
+    const [phoneField, setPhoneField] = useState('')
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        const formData = {
+            name: nameField,
+            email: emailField,
+            phone: phoneField
+        }
+
+        setNameField('')
+        setEmailField('')
+        setPhoneField('')
+
+        console.log(formData)
+    }
+
+    return (
+        <div className={styles.container}>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <h1 className={styles.title}> Que tal receber descontos e promoções exclusivas? Cadastre seu e-mail!</h1>
+
+                <h4>Nome:</h4>
+                <input className={styles.input} type="text" name="name" id="name" placeholder="Seu Nome" value={nameField} onChange={(e) => setNameField(e.target.value)} />
+
+                <h4>Email:</h4>
+                <input className={styles.input} type="email" name="email" id="email"placeholder="Seu E-mail" value={emailField} onChange={(e) => setEmailField(e.target.value)} required />
+
+                <h4>Telefone:</h4>
+                <input className={styles.input} type="tel" name="tel" id="tel" placeholder="Seu Telefone" value={phoneField} onChange={(e) => setPhoneField(e.target.value)} required />
+
+                <button className={styles.button} type="submit">Cadastrar</button>
+            </form>
+        </div>
+    )
+}
+
+export default Formulario
